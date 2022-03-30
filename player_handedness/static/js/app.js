d45fc0a0027a9147867058eb3f6a5acaf621191e
leannarent14 <90877777+leannarent14@users.noreply.github.com>
2022-03-29 19:24:33 -0500

    buildTable function

--- player_handedness/static/js/app.js
+++ player_handedness/static/js/app.js
@@ -1,1 +1,26 @@
-!testing 
+// import the data from players.js
+const tableData = players;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Building table function
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    players.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
